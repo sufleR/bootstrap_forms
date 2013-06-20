85438c0247c2424267b35538e5291af66a14ce6b
Szymon Fracczak <szymon.fracczak@netguru.pl>
2013-06-20 22:00:18 +0200

    this works only for f.error_messages

--- lib/bootstrap_forms/helpers/nested_form_helper.rb
+++ lib/bootstrap_forms/helpers/nested_form_helper.rb
@@ -12,7 +12,17 @@ begin
       module NestedFormHelper
         def bootstrap_nested_form_for(*args, &block)
           options = args.extract_options!.reverse_merge(:builder => NestedForm::TwitterBootstrapBuilder)
-          form_for(*(args << options), &block) << after_nested_form_callbacks
+          form_options = options.deep_dup
+          options[:summary_errors] = true unless form_options.has_key?(:summary_errors)
+          form_options.delete(:summary_errors)
+
+          form_for(*(args << form_options)) do |f|
+            if f.object.respond_to?(:errors) and options[:summary_errors]
+              f.error_messages.html_safe + capture(f, &block).html_safe
+            else
+              capture(f, &block).html_safe
+            end
+          end << after_nested_form_callbacks
         end
       end
     end
